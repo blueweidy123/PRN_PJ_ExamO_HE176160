97cd673acea33dbdb1993a592b62f155cd506853
antthe176160 <anlalahello@gmail.com>
2023-07-04 17:19:12 +0700

    course-view

---
+++ PRN_ExamO_HE176160/PRN_ExamO_HE176160/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN_ExamO_HE176160.Models;
+
+namespace PRN_ExamO_HE176160.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (OnlineEnExamContext context = new OnlineEnExamContext())
+            {
+                ViewBag.Exams = context.Exams;
+
+                var Exams = context.Exams.ToList();
+
+                return View(Exams);
+            }
+        }
+    }
+}
--- PRN_ExamO_HE176160/PRN_ExamO_HE176160/Program.cs
+++ PRN_ExamO_HE176160/PRN_ExamO_HE176160/Program.cs
@@ -5,9 +5,15 @@ namespace PRN_ExamO_HE176160
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/", () => "Hello World!");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}"
+                );
+
 
             app.Run();
         }
