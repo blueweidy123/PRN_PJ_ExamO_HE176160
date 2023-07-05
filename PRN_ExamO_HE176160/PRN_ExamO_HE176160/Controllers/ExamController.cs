56b36a61c8600eb5f6746b4317103a520078f5bb
antthe176160 <anlalahello@gmail.com>
2023-07-05 19:29:03 +0700

    question navigate, markup progress

--- PRN_ExamO_HE176160/PRN_ExamO_HE176160/Controllers/ExamController.cs
+++ PRN_ExamO_HE176160/PRN_ExamO_HE176160/Controllers/ExamController.cs
@@ -7,12 +7,12 @@ namespace PRN_ExamO_HE176160.Controllers
     {
         public IActionResult TakeExam(String ExamId)
         {
-            ViewBag.ExamId = ExamId;
-
-
-
             using (var context = new OnlineEnExamContext())
             {
+
+                ViewBag.ExamId = ExamId;
+                ViewBag.ExamName = context.Exams.FirstOrDefault(e => e.ExamId == ExamId).Description;
+
                 List<Question> questions = context.Questions.Where(q => q.ExamId == ExamId).ToList();
 
                 List<Option> options = context.Options.Where(o => questions.Select(q => q.QuestionId).Contains(o.QuestionId)).ToList();
