9cb42f6f3027b57b7fd624868272d835ed99f85e
antthe176160 <anlalahello@gmail.com>
2023-07-08 19:12:18 +0700

    fix user answer

--- PRN_ExamO_HE176160/PRN_ExamO_HE176160/Controllers/ExamController.cs
+++ PRN_ExamO_HE176160/PRN_ExamO_HE176160/Controllers/ExamController.cs
@@ -34,6 +34,29 @@ namespace PRN_ExamO_HE176160.Controllers
             }
         }
 
+        //[HttpPost]
+        //public IActionResult SubmitExam(List<UserAnswer> userAnswers)
+        //{
+        //    Debug.WriteLine("Selected options:");
+
+        //    using (var context = new OnlineEnExamContext())
+        //    {
+        //        foreach (UserAnswer userAnswer in userAnswers)
+        //        {
+        //            // test
+        //            Debug.WriteLine($"Question ID: {userAnswer.QuestionId}");
+        //            Debug.WriteLine($"Exam ID: {userAnswer.ExamId}");
+        //            Debug.WriteLine($"Selected Option ID: {userAnswer.SelectedOptionId}");
+
+        //            context.UserAnswers.Add(userAnswer);
+        //        }
+
+        //        context.SaveChanges();
+        //    }
+
+        //    return RedirectToAction("Index", "Dashboard");
+        //}
+
         [HttpPost]
         public IActionResult SubmitExam(List<UserAnswer> userAnswers)
         {
@@ -49,12 +72,51 @@ namespace PRN_ExamO_HE176160.Controllers
                     Debug.WriteLine($"Selected Option ID: {userAnswer.SelectedOptionId}");
 
                     context.UserAnswers.Add(userAnswer);
+
+                    // Calculate and save the mark
+                    CalculateAndSaveMark(context, userAnswer);
                 }
 
                 context.SaveChanges();
             }
 
             return RedirectToAction("Index", "Dashboard");
+        }
+
+        private void CalculateAndSaveMark(OnlineEnExamContext context, UserAnswer userAnswer)
+        {
+            // Get the exam associated with the user's answer
+            var exam = context.Exams.SingleOrDefault(e => e.ExamId == userAnswer.ExamId);
+
+            if (exam == null)
+            {
+                // Handle the case where the exam is not found
+                // You can throw an exception or return an error message
+                return;
+            }
+
+            // Get all the options for the exam
+            var options = context.Options.Where(o => o.Question.ExamId == exam.ExamId).ToList();
+
+            // Get the correct options count for the exam
+            int correctOptionsCount = options.Count(o => o.IsCorrectOption == true);
+
+            // Get the user's selected options count for the question
+            int userSelectedOptionsCount = userAnswer.SelectedOptionId != null ? 1 : 0;
+
+            // Calculate the mark
+            double mark = userSelectedOptionsCount <= 1 ? (double)userSelectedOptionsCount / correctOptionsCount : 0;
+
+            // Create a new result object
+            var result = new Result
+            {
+                UserId = userAnswer.UserId,
+                ExamId = userAnswer.ExamId,
+                Marks = (int)mark
+            };
+
+            // Add the result to the context
+            context.Results.Add(result);
         }
 
 
